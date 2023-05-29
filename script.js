3be68fec5f3a87df2102056ae41ebfc773473e69
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-05-29 20:42:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let container = document.getElementById("line");
+        let span = document.createElement("span");
+        span.className = "rotate";
+        container.appendChild(span);
